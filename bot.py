d393e677c1f17275a7a2c150e33a6ac4cdb4a24d
SHRYEAS ASHOK DESAI <42400202+Shreyasdesai4567@users.noreply.github.com>
2026-03-06 08:30:38 +0530

    Implement Telegram bot to send deals every hour

---
+++ bot.py
@@ -0,0 +1,53 @@
+import requests
+import time
+import random
+
+BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN"
+CHANNEL_USERNAME = "@DealsYeti"
+
+deals = [
+{
+"title":"🔥 Boat Airdopes 141",
+"price":"₹399",
+"link":"https://amzn.to/testdeal",
+"discount":"72%"
+},
+{
+"title":"🔥 Noise Smartwatch",
+"price":"₹1299",
+"link":"https://amzn.to/testdeal",
+"discount":"55%"
+}
+]
+
+def send_deal(deal):
+
+    message = f"""
+🔥 {deal['discount']} OFF
+
+🛒 {deal['title']}
+
+💰 Price: {deal['price']}
+
+⭐ Best Deal Today
+
+Buy Now 👇
+{deal['link']}
+"""
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHANNEL_USERNAME,
+        "text": message
+    }
+
+    requests.post(url, data=payload)
+
+while True:
+
+    deal = random.choice(deals)
+
+    send_deal(deal)
+
+    time.sleep(3600)
